9a2e1cd2818dffd1e97202bb6ab5df3b055e27e9
Laisses <laisse@gmail.com>
2022-11-24 20:21:05 -0300

    feat: add basic structure to the AccountPage

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,5 +1,57 @@
+import { useContext } from "react";
+import UsernameContext from "../contexts/UsernameContext";
+import styled from "styled-components";
+
 const AccountPage = () => {
-    return <>Olá</>
+    const [username] = useContext(UsernameContext);
+
+    return (
+        <div>
+            <div className="menu-lateral">
+                <ul>
+                    <li>
+                        <a>
+                            <img />
+                            <p>Meus Pedidos</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img />
+                            <p>Meus Cartões de crédito</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img />
+                            <p>Meus Endereços</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img />
+                            <p>Meus Dados</p>
+                        </a>
+                    </li>
+                </ul>
+                <button>Sair</button>
+            </div>
+            <div className="main-conteudo">
+                <h2>Olá {username}</h2>
+                <p>Aqui é onde você poderá visualizar e editar todos os dados da sua conta.</p>
+                <div>
+                    <img />
+                    <p>Minha conta: onde você pode consultar as informações da sua conta.</p>
+                </div>
+                <div>
+                    <img />
+                    <p>Sacola: veja os produtos que você já adicionou à sua sacola.</p>
+                </div>
+            </div>
+        </div>
+    );
 };
+
+const AccountContainer = styled.div``;
 
 export default AccountPage;

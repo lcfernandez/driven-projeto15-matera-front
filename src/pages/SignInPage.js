f82893ef7534e3b085fb2b32e7c37bba790c605b
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-24 11:42:43 -0300

    feat: integrate with API

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,11 +1,40 @@
 import FormStyle from "../assets/styles/FormStyle";
 import SignPageStyle from "../assets/styles/SignPageStyle";
 
+import { BASE_URL } from "../constants/url.js";
+
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignInPage() {
+    const [disabled, setDisabled] = useState(false);
+    const [email, setEmail] = useState(undefined);
+    const [password, setPassword] = useState(undefined);
+
     function signIn(e) {
         e.preventDefault();
+
+        setDisabled(true);
+
+        const body = {
+            email,
+            password
+        };
+
+        axios
+            .post(`${BASE_URL}/sign-in`, body)
+            .then(
+                res => {
+                    console.log(res.data);
+                }
+            )
+            .catch(
+                err => {
+                    alert(err.response.data.message);
+                    setDisabled(false);
+                }
+            );
     }
 
     return (
@@ -16,13 +45,17 @@ export default function SignInPage() {
 
             <FormStyle onSubmit={signIn}>
                 <input
+                    disabled={disabled && true}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="E-mail"
                     required
                     type="email"
                 />
                 
                 <input
+                    disabled={disabled && true}
                     minLength="4"
+                    onChange={(e) => setPassword(e.target.value)}
                     placeholder="Senha"
                     required
                     type="password"

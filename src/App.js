684ad152d2a255fbe5c397a34eef849e67e4180d
Laisses <laisse@gmail.com>
2022-11-24 11:22:35 -0300

    feat: add products routes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import SignInPage from "./pages/SignInPage";
 import SignUpPage from "./pages/SignUpPage";
+import ProductsPage from "./pages/ProductsPage";
+import BedsPage from "./pages/BedsPage";
+import ChairsPage from "./pages/ChairsPage";
+import SofasPage from "./pages/SofasPage";
+import TablesPage from "./pages/TablesPage";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -20,6 +25,11 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/cadastro" element={<SignUpPage />} />
                 <Route path="/login" element={<SignInPage />} />
+                <Route path="/produtos" element={<ProductsPage />} />
+                <Route path="/produtos/cadeiras" element={<ChairsPage />} />
+                <Route path="/produtos/camas" element={<BedsPage />} />
+                <Route path="/produtos/sofas" element={<SofasPage />} />
+                <Route path="/produtos/mesas" element={<TablesPage />} />
             </Routes>
 
             <Footer />
---
+++ src/pages/TablesPage.js
@@ -0,0 +1,87 @@
+import { ProductsTitle } from "../components/ProductsTitle";
+import { ProductsContainer, ProductsUl, ListItem } from "../common.js/common";
+import { SideMenu } from "../components/SideMenu";
+import { useEffect, useState } from "react";
+
+const produtos = [
+    {
+        _id: "637e8782e6cb1ac3c3a48f63",
+        name: "Cama Turquesa",
+        price: "12300",
+        date: "24/11/2022",
+        image: "https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZnVybml0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+        type: "cama"
+    },
+    {
+        _id: "637f2679e6cb1ac3c3a48f70",
+        name: "Sofa Platao",
+        price: "139910",
+        date: "24/11/2022",
+        image: "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnVybml0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+        type: "sofa"
+    },
+    {
+        _id: "637f26ace6cb1ac3c3a48f71",
+        name: "Sofa Sócrates",
+        price: "34990",
+        date: "24/11/2022",
+        image: "https://images.unsplash.com/photo-1592078615290-033ee584e267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnVybml0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+        type: "sofa"
+    },
+];
+
+export const TablesPage = () => {
+    const [products, setProducts] = useState(undefined);
+
+    const filterProduct = products => {
+        const tables = products.filter(p => p.type === "mesa");
+        setProducts(tables);
+    };
+
+    useEffect(() => {
+        filterProduct(produtos);
+    }, []);
+
+    const ListOfProducts = ({ name, price, image }) => {
+        return (
+            <ListItem>
+                <img
+                    alt="imagem do produto"
+                    src={image}
+                />
+                <h3>{name}</h3>
+                <p>{price}</p>
+            </ListItem>
+        );
+    };
+
+    const ProductsDisplay = () => {
+        if (products === undefined) {
+            return <div>Loading...</div>;
+        }
+
+        return (
+            <ProductsUl>
+                {products.map(p => <ListOfProducts
+                    key={p._id}
+                    id={p._id}
+                    name={p.name}
+                    price={p.price}
+                    image={p.image}
+                />)}
+            </ProductsUl>
+        );
+    };
+
+    return (
+        <>
+            <ProductsTitle />
+            <ProductsContainer>
+                <SideMenu />
+                <ProductsDisplay />
+            </ProductsContainer>
+        </>
+    );
+};
+
+export default TablesPage;

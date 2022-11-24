76a406cafae1661ebe75e1151c551a0a2166b8eb
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-24 00:17:58 -0300

    feat: implement sign in page

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,7 +1,39 @@
+import FormStyle from "../assets/styles/FormStyle";
+import SignPageStyle from "../assets/styles/SignPageStyle";
+
+import { Link } from "react-router-dom";
+
 export default function SignInPage() {
+    function signIn(e) {
+        e.preventDefault();
+    }
+
     return (
-        <>
-            Login
-        </>
+        <SignPageStyle>
+            <div>
+                Login
+            </div>
+
+            <FormStyle onSubmit={signIn}>
+                <input
+                    placeholder="E-mail"
+                    required
+                    type="email"
+                />
+                
+                <input
+                    minLength="4"
+                    placeholder="Senha"
+                    required
+                    type="password"
+                />
+
+                <button>Entrar</button>
+            </FormStyle>
+
+            <Link to="/cadastro">
+                Não tem uma conta? Cadastre-se agora!
+            </Link>
+        </SignPageStyle>
     );
 }

82f4acaf4c81a8dc280e7c34bf26787472d24d5e
Laisses <laisse@gmail.com>
2022-11-24 22:18:53 -0300

    feat: add icons to the accountPage

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,41 +1,66 @@
 import { useContext } from "react";
 import UsernameContext from "../contexts/UsernameContext";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import card from "../assets/images/card-outline.png";
+import gift from "../assets/images/gift-outline.png";
+import home from "../assets/images/home-outline.png";
+import logOut from "../assets/images/log-out-outline.png";
+import person from "../assets/images/person-outline.png";
 
 const AccountPage = () => {
     const [username] = useContext(UsernameContext);
 
     return (
-        <div>
-            <div className="menu-lateral">
+        <AccountContainer>
+            <AccountMenu className="menu-lateral">
                 <ul>
                     <li>
-                        <a>
-                            <img />
+                        <AccountLinks>
+                            <img
+                                alt="ícone de pedidos"
+                                src={gift}
+                            />
                             <p>Meus Pedidos</p>
-                        </a>
+                        </AccountLinks>
                     </li>
                     <li>
-                        <a>
-                            <img />
+                        <AccountLinks>
+                            <img
+                                alt="ícone de cartão"
+                                src={card}
+                            />
                             <p>Meus Cartões de crédito</p>
-                        </a>
+                        </AccountLinks>
                     </li>
                     <li>
-                        <a>
-                            <img />
+                        <AccountLinks>
+                            <img
+                                alt="ícone de endereço"
+                                src={home}
+                            />
                             <p>Meus Endereços</p>
-                        </a>
+                        </AccountLinks>
                     </li>
                     <li>
-                        <a>
-                            <img />
+                        <AccountLinks>
+                            <img
+                                alt="ícone de pessoa"
+                                src={person}
+                            />
                             <p>Meus Dados</p>
-                        </a>
+                        </AccountLinks>
                     </li>
                 </ul>
-                <button>Sair</button>
-            </div>
+                <Button>
+                    <img
+                        alt="ícone de sair"
+                        src={logOut}
+                    />
+                    <p>Sair</p>
+                </Button>
+            </AccountMenu>
             <div className="main-conteudo">
                 <h2>Olá {username}</h2>
                 <p>Aqui é onde você poderá visualizar e editar todos os dados da sua conta.</p>
@@ -48,10 +73,29 @@ const AccountPage = () => {
                     <p>Sacola: veja os produtos que você já adicionou à sua sacola.</p>
                 </div>
             </div>
-        </div>
+        </AccountContainer>
     );
 };
 
-const AccountContainer = styled.div``;
+const AccountContainer = styled.div`
+    display: flex;
+`;
+
+const AccountMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    img{
+        width: 24px;
+        height: 24px;
+    }
+`;
+
+const AccountLinks = styled(Link)`
+    display: flex;
+`;
+const Button = styled.button`
+    display: flex;
+`;
+//24px
 
 export default AccountPage;

066af2539ae50b1d3228f6793dabe442ceb0dd50
Laisses <laisse@gmail.com>
2022-11-24 22:28:02 -0300

    feat: create AccountMenu component

---
+++ src/components/AccountMenu.js
@@ -0,0 +1,59 @@
+import card from "../assets/images/card-outline.png";
+import gift from "../assets/images/gift-outline.png";
+import home from "../assets/images/home-outline.png";
+import logOut from "../assets/images/log-out-outline.png";
+import person from "../assets/images/person-outline.png";
+
+import { AccountMenuContainer, AccountLinks, Button } from "../assets/styles/AccountStyle";
+
+export const AccountMenu = () => {
+    return (
+        <AccountMenuContainer>
+            <ul>
+                <li>
+                    <AccountLinks>
+                        <img
+                            alt="ícone de pedidos"
+                            src={gift}
+                        />
+                        <p>Meus Pedidos</p>
+                    </AccountLinks>
+                </li>
+                <li>
+                    <AccountLinks>
+                        <img
+                            alt="ícone de cartão"
+                            src={card}
+                        />
+                        <p>Meus Cartões de crédito</p>
+                    </AccountLinks>
+                </li>
+                <li>
+                    <AccountLinks>
+                        <img
+                            alt="ícone de endereço"
+                            src={home}
+                        />
+                        <p>Meus Endereços</p>
+                    </AccountLinks>
+                </li>
+                <li>
+                    <AccountLinks>
+                        <img
+                            alt="ícone de pessoa"
+                            src={person}
+                        />
+                        <p>Meus Dados</p>
+                    </AccountLinks>
+                </li>
+            </ul>
+            <Button>
+                <img
+                    alt="ícone de sair"
+                    src={logOut}
+                />
+                <p>Sair</p>
+            </Button>
+        </AccountMenuContainer>
+    );
+};
--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -3,64 +3,15 @@ import UsernameContext from "../contexts/UsernameContext";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import card from "../assets/images/card-outline.png";
-import gift from "../assets/images/gift-outline.png";
-import home from "../assets/images/home-outline.png";
-import logOut from "../assets/images/log-out-outline.png";
-import person from "../assets/images/person-outline.png";
+import { AccountContainer } from "../assets/styles/AccountStyle";
+import { AccountMenu } from "../components/AccountMenu";
 
 const AccountPage = () => {
     const [username] = useContext(UsernameContext);
 
     return (
         <AccountContainer>
-            <AccountMenu className="menu-lateral">
-                <ul>
-                    <li>
-                        <AccountLinks>
-                            <img
-                                alt="ícone de pedidos"
-                                src={gift}
-                            />
-                            <p>Meus Pedidos</p>
-                        </AccountLinks>
-                    </li>
-                    <li>
-                        <AccountLinks>
-                            <img
-                                alt="ícone de cartão"
-                                src={card}
-                            />
-                            <p>Meus Cartões de crédito</p>
-                        </AccountLinks>
-                    </li>
-                    <li>
-                        <AccountLinks>
-                            <img
-                                alt="ícone de endereço"
-                                src={home}
-                            />
-                            <p>Meus Endereços</p>
-                        </AccountLinks>
-                    </li>
-                    <li>
-                        <AccountLinks>
-                            <img
-                                alt="ícone de pessoa"
-                                src={person}
-                            />
-                            <p>Meus Dados</p>
-                        </AccountLinks>
-                    </li>
-                </ul>
-                <Button>
-                    <img
-                        alt="ícone de sair"
-                        src={logOut}
-                    />
-                    <p>Sair</p>
-                </Button>
-            </AccountMenu>
+            <AccountMenu />
             <div className="main-conteudo">
                 <h2>Olá {username}</h2>
                 <p>Aqui é onde você poderá visualizar e editar todos os dados da sua conta.</p>
@@ -76,26 +27,5 @@ const AccountPage = () => {
         </AccountContainer>
     );
 };
-
-const AccountContainer = styled.div`
-    display: flex;
-`;
-
-const AccountMenu = styled.div`
-    display: flex;
-    flex-direction: column;
-    img{
-        width: 24px;
-        height: 24px;
-    }
-`;
-
-const AccountLinks = styled(Link)`
-    display: flex;
-`;
-const Button = styled.button`
-    display: flex;
-`;
-//24px
 
 export default AccountPage;

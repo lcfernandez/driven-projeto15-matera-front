adaa3992ca66efb38b4decf43e5590fa8edc4017
Laisses <laisse@gmail.com>
2022-11-26 18:32:19 -0300

    feat: add form to add address

--- src/pages/AddressesPage.js
+++ src/pages/AddressesPage.js
@@ -1,15 +1,18 @@
 import { AccountContainer } from "../assets/styles/AccountStyle";
 import { AccountMenu } from "../components/AccountMenu";
 import close from "../assets/images/close-outline.png";
-import { AddressesContainer, AddressesList, AddressListItem } from "../assets/styles/AddressesStyles";
+import open from "../assets/images/add-circle-outline.png";
+import { AddressesContainer, AddressesList, AddressListItem, AddressFormContainer, AddressForm, TextLabel, TextInput } from "../assets/styles/AddressesStyles";
 import { useContext, useEffect, useState } from "react";
 import TokenContext from "../contexts/TokenContext";
 import { BASE_URL } from "../constants/url";
 import axios from "axios";
 
 const AddressesPage = () => {
-    const [addresses , setAddresses] = useState(undefined);
+    const [addresses, setAddresses] = useState(undefined);
+    const [showForm, setShowForm] = useState(false);
     const [token] = useContext(TokenContext);
+    const [form, setForm] = useState({ firstName: "", lastName: "", address: "", cep: "", number: "", complement: "", district: "", city: "", estate: "", phone: "" });
 
     useEffect(() => {
         getAddresses(token);
@@ -49,6 +52,19 @@ const AddressesPage = () => {
         );
     };
 
+    const setFormStatus = () => {
+        if (showForm === false) {
+            setShowForm(true);
+        } else {
+            setShowForm(false);
+        }
+    };
+
+    const handleForm = e => {
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
+
     return (
         <AccountContainer>
             <AccountMenu />
@@ -64,6 +80,124 @@ const AddressesPage = () => {
                         />)
                     }
                 </AddressesList>
+                <AddressFormContainer>
+                    <p>Adicione um novo endereço</p>
+                    <button onClick={setFormStatus}>
+                        <img
+                            alt="ícone de adicionar"
+                            src={open}
+                        />
+                    </button>
+                    {showForm
+                        ?
+                        <AddressForm>
+                            <TextLabel htmlFor="cep">CEP</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="cep"
+                                name="cep"
+                                value={form.cep}
+                                onChange={handleForm}
+                                placeholder="Digite o CEP"
+                                required
+                            />
+                            <TextLabel htmlFor="firstName">Nome</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                value={form.firstName}
+                                onChange={handleForm}
+                                placeholder="Digite o nome"
+                            />
+                            <TextLabel htmlFor="lastName">Sobrenome</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="lastName"
+                                name="lastName"
+                                value={form.lastName}
+                                onChange={handleForm}
+                                placeholder="Digite o sobrenome"
+                                required
+                            />
+                            <TextLabel htmlFor="address">Endereço</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="address"
+                                name="address"
+                                value={form.address}
+                                onChange={handleForm}
+                                placeholder="Digite o endereço"
+                                required
+                            />
+                            <TextLabel htmlFor="number">Número</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="number"
+                                name="number"
+                                value={form.number}
+                                onChange={handleForm}
+                                placeholder="Digite o número"
+                                required
+                            />
+                            <TextLabel htmlFor="complement">Complemento (opcional)</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="complement"
+                                name="complement"
+                                value={form.complement}
+                                onChange={handleForm}
+                                placeholder="Complemento..."
+                                required
+                            />
+                            <TextLabel htmlFor="district">Bairro</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="district"
+                                name="district"
+                                value={form.district}
+                                onChange={handleForm}
+                                placeholder="Digite o bairro"
+                                required
+                            />
+                            <TextLabel htmlFor="city">Cidade</TextLabel>
+                            <TextInput
+                                type="text"
+                                id="city"
+                                name="city"
+                                value={form.city}
+                                onChange={handleForm}
+                                placeholder="Digite a cidade"
+                                required
+                            />
+                            <TextLabel htmlFor="estate">Estado</TextLabel>
+                            <p>ex: SP</p>
+                            <TextInput
+                                type="text"
+                                id="estate"
+                                name="estate"
+                                value={form.estate}
+                                onChange={handleForm}
+                                placeholder="Digite o estado"
+                                required
+                            />
+                            <TextLabel htmlFor="phone">Celular</TextLabel>
+                            <p>ex: (11) 9 98768765</p>
+                            <TextInput
+                                type="text"
+                                id="phone"
+                                name="phone"
+                                value={form.phone}
+                                onChange={handleForm}
+                                placeholder="Digite o celular"
+                                required
+                            />
+                            <button >Cadastrar</button>
+                        </AddressForm>
+                        :
+                        ""
+                    }
+                </AddressFormContainer>
             </AddressesContainer>
         </AccountContainer>
     );

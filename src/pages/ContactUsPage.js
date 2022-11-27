2c60d907931ec4845f8c4fa9655e706681b0ac0c
Laisses <laisse@gmail.com>
2022-11-27 09:43:06 -0300

    feat: create state to define type of help to be shown

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -4,20 +4,32 @@ import help from "../assets/images/help-circle-outline.svg";
 import whatsapp from "../assets/images/logo-whatsapp.svg";
 import swap from "../assets/images/swap-horizontal-outline.svg";
 import { ContactImg, ContactTitle } from "../assets/styles/ContactUsStyles";
+import { useState } from "react";
 
 const ContactUsPage = () => {
+    const options = {
+        message:false,
+        faq: false,
+        changes: false,
+        chat:false,
+        whatsapp: false,
+        default: true
+    };
+
+    const [help, setHelp] = useState(options);
+
     return (
         <div className="container">
             <ContactImg />
             <ContactTitle />
             <div>
                 <div className="side-menu">
-                <ul>
-            <li>
-                <img/>
-                <p></p>
-            </li>
-            </ul>
+                    <ul>
+                        <li>
+                            <img />
+                            <p></p>
+                        </li>
+                    </ul>
                 </div>
                 <div className="maininfo"></div>
             </div>

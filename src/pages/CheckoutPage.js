28c1f5d5f0031c7891daf3fbe56b0fab031a4b99
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-27 16:46:48 -0300

    feat: list address and cards in checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,12 +1,36 @@
 import { BASE_URL } from "../constants/url";
 
 import CartContext from "../contexts/CartContext";
+import TokenContext from "../contexts/TokenContext";
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function CheckoutPage({ shipping, zipCode }) {
+    const [addresses, setAddresses] = useState([]);
+    const [cards, setCards] = useState([]);
     const [cart] = useContext(CartContext);
+    const [token] = useContext(TokenContext);
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        };
+        
+        axios
+            .get(`${BASE_URL}/addresses`, config)
+            .then(res => setAddresses(res.data))
+            .catch(err => console.error(err.response.data.message || err.response.data));
+
+        axios
+            .get(`${BASE_URL}/cards`, config)
+            .then(res => setCards(res.data))
+            .catch(err => console.error(err.response.data.message || err.response.data));
+    }, []);
     
     return (
         <CheckoutPageContainer>
@@ -21,19 +45,38 @@ export default function CheckoutPage({ shipping, zipCode }) {
                             <th><img alt={`imagem de ${item.name}`} src={`${BASE_URL}${item.image}`} /></th>
                             <th>{item.name}</th>
                             <th>Qtd. {item.qtd}</th>
-                            <th>{item.sumPrice}</th>
+                            <th>R$ {item.sumPrice}</th>
                         </tr>
-            )}
+                    )}
                 </tbody>
             </table>
             
-            {zipCode}
-            {shipping}
+            {addresses.length > 0 ? addresses.map((address, index) =>
+                <button key={address._id}>
+                    <p>{`${address.firstName} ${address.lastName}`}</p>
+                    <p>{`${address.address}, ${address.number} - ${address.complement}`}</p>
+                    <p>{`${address.district} - ${address.city} - ${address.estate}`}</p>
+                    <p>{`${address.cep}`}</p>
+                </button>
+            ) : "Você ainda não tem endereços cadastrados."}
+            <Link to="/conta/enderecos">Cadastrar/editar endereço</Link>
+
+            {cards.length > 0 ? cards.map((card, index) =>
+                <button key={card._id}>
+                    <p>{`${card.number}`}</p>
+                </button>
+            ) : "Você ainda não tem cartões cadastrados."}
+            <Link to="/conta/cartoes">Cadastrar/editar cartões</Link>
+            
+            <div>{zipCode}</div>
+            <div>{shipping}</div>
         </CheckoutPageContainer>
     );
 }
 
 const CheckoutPageContainer = styled.div`
+    text-align: left;
+
     img {
         width: 100px;
     }

98b4db36481f14fefb6dde5d48b19e818e04d90b
Laisses <laisse@gmail.com>
2022-11-27 09:20:27 -0300

    style: import images from assets

--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,3 +1,9 @@
+import contact from "../assets/images/contact.png";
+import chatbubble from "../assets/images/chatbubble-ellipses-outline.svg";
+import help from "../assets/images/help-circle-outline.svg";
+import whatsapp from "../assets/images/logo-whatsapp.svg";
+import swap from "../assets/images/swap-horizontal-outline.svg";
+
 const ContactUsPage = () => {
     return <>oi</>
 };

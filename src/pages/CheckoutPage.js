46409f71a06139306b2015d399dde5cbb2fc86e3
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-27 14:02:35 -0300

    feat: list items in checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,17 +1,40 @@
+import { BASE_URL } from "../constants/url";
+
 import CartContext from "../contexts/CartContext";
 
 import { useContext } from "react";
+import styled from "styled-components";
 
 export default function CheckoutPage({ shipping, zipCode }) {
     const [cart] = useContext(CartContext);
     
     return (
-        <>
-            Pedido
+        <CheckoutPageContainer>
+            <div>
+                Pedido
+            </div>
 
-            {cart.map(item => item.name)}
+            <table>
+                <tbody>    
+                    {cart.map((item, index) =>
+                        <tr key={index}>
+                            <th><img alt={`imagem de ${item.name}`} src={`${BASE_URL}${item.image}`} /></th>
+                            <th>{item.name}</th>
+                            <th>Qtd. {item.qtd}</th>
+                            <th>{item.sumPrice}</th>
+                        </tr>
+            )}
+                </tbody>
+            </table>
+            
             {zipCode}
             {shipping}
-        </>
+        </CheckoutPageContainer>
     );
 }
+
+const CheckoutPageContainer = styled.div`
+    img {
+        width: 100px;
+    }
+`;

beedbca4052f5286c29e232ea513a64f8a6df7e1
Laisses <laisse@gmail.com>
2022-11-27 08:14:01 -0300

    style: add images to about us page

---
+++ src/assets/styles/AboutUsStyles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div``;
+
+export const AboutImg = styled.img`
+    width: 100%;
+`;
+
+export const ConfortContainer = styled.div``;
+
+export const ConfortImg = styled.img``;
+
+export const SustainabilityContainer = styled.div``;
+
+export const SustainabilityImg = styled.img``;

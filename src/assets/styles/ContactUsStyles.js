9467d2cab00a076f3d1b61c915ae324b46303e05
Laisses <laisse@gmail.com>
2022-11-27 09:24:06 -0300

    feat: create basic jsx structure

--- src/assets/styles/ContactUsStyles.js
+++ src/assets/styles/ContactUsStyles.js
@@ -1,1 +1,5 @@
 import styled from "styled-components";
+
+export const ContactImg = styled.img``;
+
+export const ContactTitle = styled.h1``;
--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -3,9 +3,24 @@ import chatbubble from "../assets/images/chatbubble-ellipses-outline.svg";
 import help from "../assets/images/help-circle-outline.svg";
 import whatsapp from "../assets/images/logo-whatsapp.svg";
 import swap from "../assets/images/swap-horizontal-outline.svg";
+import { ContactImg, ContactTitle } from "../assets/styles/ContactUsStyles";
 
 const ContactUsPage = () => {
-    return <>oi</>
+    return (
+        <div className="container">
+            <ContactImg />
+            <ContactTitle />
+            <ul>
+            <li>
+                <img/>
+                <div>
+                    <h2></h2>
+                    <p></p>
+                </div>
+            </li>
+            </ul>
+        </div>
+    );
 };
 
 export default ContactUsPage;

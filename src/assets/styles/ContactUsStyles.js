b676876c19072cb7aa64c954113974b8df9001ea
Laisses <laisse@gmail.com>
2022-11-27 09:53:08 -0300

    style: create basic styled containers

--- src/assets/styles/ContactUsStyles.js
+++ src/assets/styles/ContactUsStyles.js
@@ -1,7 +1,21 @@
 import styled from "styled-components";
 
+export const ContactContainer = styled.div``;
+
 export const ContactImg = styled.img`
     width: 100%;
 `;
 
 export const ContactTitle = styled.h1``;
+
+export const MenuContainer = styled.div``;
+
+export const MenuList = styled.ul`
+    img {
+        width: 20px;
+    }
+`;
+
+export const DetailsContainer = styled.div``;
+
+export const MainContainer = styled.div``;
--- src/pages/ContactUsPage.js
+++ src/pages/ContactUsPage.js
@@ -1,9 +1,10 @@
 import contact from "../assets/images/contact.png";
-import chatbubble from "../assets/images/chatbubble-ellipses-outline.svg";
-import help from "../assets/images/help-circle-outline.svg";
+import chat from "../assets/images/chatbubble-ellipses-outline.svg";
+import faq from "../assets/images/help-circle-outline.svg";
+import message from "../assets/images/mail-outline.svg"
 import whatsapp from "../assets/images/logo-whatsapp.svg";
 import swap from "../assets/images/swap-horizontal-outline.svg";
-import { ContactImg, ContactTitle } from "../assets/styles/ContactUsStyles";
+import { ContactContainer, ContactImg, ContactTitle, MenuContainer, MenuList, DetailsContainer, MainContainer } from "../assets/styles/ContactUsStyles";
 import { useState } from "react";
 
 const ContactUsPage = () => {
@@ -19,22 +20,17 @@ const ContactUsPage = () => {
     const [help, setHelp] = useState(options);
 
     return (
-        <div className="container">
+        <ContactContainer>
             <ContactImg />
             <ContactTitle />
-            <div>
-                <div className="side-menu">
-                    <ul>
-                        <li>
-                            <img />
-                            <p></p>
-                        </li>
-                    </ul>
-                </div>
-                <div className="maininfo"></div>
-            </div>
+            <MainContainer>
+                <MenuContainer>
 
-        </div>
+                </MenuContainer>
+                <DetailsContainer></DetailsContainer>
+            </MainContainer>
+
+        </ContactContainer>
     );
 };
 
